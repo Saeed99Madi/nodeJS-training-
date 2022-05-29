105b4ebd11a5cd1fa6b7b380b684d50f5f697a3a
Saeed99Madi <saed.zooro@gmail.com>
2022-05-29 17:12:22 +0300

    try some OS module function

---
+++ referance/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+//platform
+console.log(os.platform()); 
+
+// CPU Arch
+console.log(os.arch());
+
+//CPU core info 
+console.log(os.cpus());
+
+//Free memory 
+console.log(os.freemem());
+
+
+//total memory 
+console.log(os.totalmem());
+//Home Directory 
+console.log(os.homedir());
+
+//Up time 
+console.log( os.uptime());
