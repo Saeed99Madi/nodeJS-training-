a32542842f91b51f7dbd966c3b35abcc71bbbb82
Saeed99Madi <saed.zooro@gmail.com>
2022-05-29 16:19:21 +0300

    make edit in fs-demo to try rename, write, read in file

--- referance/fs-demo.js
+++ referance/fs-demo.js
@@ -14,7 +14,33 @@ const path = require('path');
 
 // create and write file 
 
-fs.writeFile(path.join(__dirname,'/test','hello.txt'),'Hello World!', err =>{
-    if(err) throw err;
-    console.log('File written to...');
-});
+// fs.writeFile(path.join(__dirname,'/test','hello.txt'),'Hello World!', err =>{
+//     if(err) throw err;
+//     console.log('File written to...');
+// });
+
+
+// fs.writeFile(path.join(__dirname,'/test','hello.txt'),'Hello World!', err =>{
+//     if(err) throw err;
+//     console.log('File written to...');
+
+//     //File append 
+//     fs.appendFile(path.join(__dirname,'/test','hello.txt'),'I love NodeJS', err =>{
+//         if(err) throw err;
+//         console.log('File written to...');
+//     });
+// });
+
+        //Read File 
+        // fs.readFile(path.join(__dirname,'/test','hello.txt'), 'utf-8' , (err, data) =>{
+        //     if(err) throw err;
+        //     console.log(data,' File have been readed ...');
+        // });
+
+        //rename file 
+         //Read File 
+         fs.rename(path.join(__dirname,'/test','hello.txt'), path.join(__dirname,'/test','helloWorld.txt'), (err) =>{
+            if(err) throw err;
+            console.log(' File have been renamed ...');
+        });
+
