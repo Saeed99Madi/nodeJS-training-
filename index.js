f04826af0e539cc877d5c5c22990837d7f4f8019
Saeed99Madi <saed.zooro@gmail.com>
2022-05-29 19:18:15 +0300

    create public folder with some html pages

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
+    if (req.url === '/' ){
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.end('<h1>Home with Saeid Mahmoud Madi</h1>');
+    }
     console.log(req.url);
 });
 const PORT = process.env.PORT || 5000;
 server.listen(PORT , ()=> console.log(`Server running on port ${PORT}`));
+
