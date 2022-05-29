42c50b197b9774f193da1f0a9f3252d78be1bf04
Saeed99Madi <saed.zooro@gmail.com>
2022-05-29 21:18:40 +0300

    make router simulation

--- index.js
+++ index.js
@@ -21,11 +21,24 @@ const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     if (req.url === '/' ){
-        res.writeHead(200,{'Content-Type': 'text/html'})
-        res.end('<h1>Home with Saeid Mahmoud Madi</h1>');
+        fs.readFile(path.join(__dirname,'public','index.html'), (err, content)=>{
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    }
+    if (req.url === '/about' ){
+        fs.readFile(path.join(__dirname,'public','about.html'), (err, content)=>{
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type': 'text/html'});
+            res.end(content);
+        })
     }
     console.log(req.url);
 });
+
+
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT , ()=> console.log(`Server running on port ${PORT}`));
 
