4523097ff5a9de07554f35a2e2a5fcdbd844fc40
Saeed99Madi <saed.zooro@gmail.com>
2022-05-29 18:29:46 +0300

    create http_demo.js file & apply logger in index file

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // console.log('Hello From Node.js ....');
 
 
-const Person = require('./person');
+// const Person = require('./person');
 
-const person1 =new Person('Saeid Madi', 24);
+// const person1 =new Person('Saeid Madi', 24);
 
-person1.greating();
+// person1.greating();
+const Logger = require('./reference/logger');
+
+const logger = new Logger();
+
+logger.on('message', (data)=>{
+    console.log('Called Listener :' , data);
+})
+logger.log('Hello World'); 
